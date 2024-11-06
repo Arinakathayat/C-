ca13ed2f5ebbaf59e4a9a4385990fdc4abf3524a
Alina Kathayat <ariasushi01@gmail.com>
2024-11-06 19:28:02 +0545

    Create hybrid.c

---
+++ hybrid.c
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Vehicle {
+public:
+    Vehicle() {
+        cout << "Vehicle created" << endl;
+    }
+    void displayVehicle() {
+        cout << "This is a Vehicle" << endl;
+    }
+};
+
+// Derived class from Vehicle (Single Inheritance)
+class Car : public Vehicle {
+public:
+    Car() {
+        cout << "Car created" << endl;
+    }
+    void displayCar() {
+        cout << "This is a Car" << endl;
+    }
+};
+
+// Another independent base class
+class Brand {
+public:
+    Brand() {
+        cout << "Brand set" << endl;
+    }
+    void displayBrand() {
+        cout << "This is a Brand" << endl;
+    }
+};
+
+// Derived class from both Car and Brand (Hybrid Inheritance: Single + Multiple Inheritance)
+class Sedan : public Car, public Brand {
+public:
+    Sedan() {
+        cout << "Sedan created" << endl;
+    }
+    void displaySedan() {
+        cout << "This is a Sedan" << endl;
+    }
+};
+
+int main() {
+    Sedan mySedan;
+    mySedan.displayVehicle();  // From Vehicle class
+    mySedan.displayCar();      // From Car class
+    mySedan.displayBrand();    // From Brand class
+    mySedan.displaySedan();    // From Sedan class
+    return 0;
+}
