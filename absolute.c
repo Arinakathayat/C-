e617c2f1ac24766536380d3acae8dd7d574518c7
Alina Kathayat <ariasushi01@gmail.com>
2024-11-20 18:35:36 +0545

    Create absolute.c

---
+++ absolute.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+using namespace std;
+Template <class T>
+T abs (Tn)
+{ 
+if (n<)
+  return(-n);
+else 
+return(n);
+}
+int main()
+{
+  int a=-4;
+float b=-9.2;
+cout<<"Absolute value of a="<<abs(a)<<endl;
+cout<<"Absolute value of b="<<abs(a)<<endl;
+return 0;
+}
